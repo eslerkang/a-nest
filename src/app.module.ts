7653b69fea31ebff07caa7e36613ad5479ce4c83
eslerkang <eslerkang@gmail.com>
2022-05-13 11:36:24 +0900

    Initial settings & Logger applied

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${process.env.NODE_ENV}.env`, '.env'],
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
